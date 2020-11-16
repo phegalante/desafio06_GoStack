38691734045a04c95fea3dd414eb6ed5157efaf2
phegalante <phegalante@gmail.com>
2020-11-16 15:11:25 -0300

    final com csv

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { getCustomRepository, getRepository } from 'typeorm';
+import multer from 'multer';
+
+import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
 
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
@@ -52,8 +57,16 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.json({ deleted: 'not found!' });
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+
+    const transactions = await importTransactions.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
